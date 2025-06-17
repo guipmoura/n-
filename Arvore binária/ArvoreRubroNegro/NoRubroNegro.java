cd413d36ae1cf68a4892051bc9d2bf17ec9453c9
guipmoura <161655718+guipmoura@users.noreply.github.com>
2025-06-17 19:29:37 -0300

    Ajustando o Nó depois da criação do Enum

--- Arvore binária/ArvoreRubroNegro/NoRubroNegro.java
+++ Arvore binária/ArvoreRubroNegro/NoRubroNegro.java
@@ -1,13 +1,10 @@
 public class NoRubroNegro {
     public int chave;
     public NoRubroNegro esquerda, direita, pai;
-    public boolean cor;
-
-    public static final boolean VERMELHO = true;
-    public static final boolean PRETO = false;
+    public Cor cor;
 
     public NoRubroNegro(int chave) {
         this.chave = chave;
-        this.cor = VERMELHO;
+        this.cor = Cor.VERMELHO;
     }
 }
